d1a94d450b58507b565e95ed6f4b6522694dd375
CL_0UD1 <633shalahuddin@gmail.com>
2024-02-04 22:18:33 +0700

    Header Finished

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,37 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
 import React from 'react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import { Input } from '../ui/input';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center gap-5 justify-between">
+        <Link href="/">
+          <Image
+            src="/img/logo.png"
+            width={36}
+            height={12}
+            alt="Nephology Logo"
+          />
+        </Link>
+        <Link href="/">Nephology</Link>
+        <Input placeholder="Search Novel"></Input>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
