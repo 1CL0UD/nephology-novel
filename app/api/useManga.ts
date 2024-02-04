2680ca95bfbce084728bfd1694526e21188a88c9
CL_0UD1 <633shalahuddin@gmail.com>
2024-02-04 23:54:08 +0700

    called 1 manga data

--- app/api/useManga.ts
+++ app/api/useManga.ts
@@ -1,7 +1,38 @@
-import useData from './useData';
+import { useEffect, useState } from 'react';
+import yumemiru from '../mock-data/yumemiru';
+import MangaData from '../types/manga';
 
-interface Manga {}
+export const fetchData = async (): Promise<MangaData> => {
+  try {
+    // const response = await apiClient.get<Data>('/manga');
+    const response = { data: yumemiru, isLoading: false, error: null };
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch data');
+  }
+};
 
-const useManga = () => useData<Manga>('/manga');
+const useManga = () => {
+  const [data, setData] = useState<MangaData | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const result = await fetchData();
+        setData(result);
+      } catch (error) {
+        setError('Failed to fetch data');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataFromAPI();
+  }, []);
+
+  return { data, loading, error };
+};
 
 export default useManga;
