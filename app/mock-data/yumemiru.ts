7e65e33cb18c29e693d52cca07c294f5e7dc7e43
CL_0UD1 <633shalahuddin@gmail.com>
2024-02-04 23:54:36 +0700

    changed mock data

---
+++ app/mock-data/yumemiru.ts
@@ -0,0 +1,153 @@
+export default {
+  data: {
+    mal_id: 148476,
+    url: 'https://myanimelist.net/manga/148476/Yumemiru_Danshi_wa_Genjitsushugisha',
+    images: {
+      jpg: {
+        image_url: 'https://cdn.myanimelist.net/images/manga/3/263462.jpg',
+        small_image_url:
+          'https://cdn.myanimelist.net/images/manga/3/263462t.jpg',
+        large_image_url:
+          'https://cdn.myanimelist.net/images/manga/3/263462l.jpg',
+      },
+      webp: {
+        image_url: 'https://cdn.myanimelist.net/images/manga/3/263462.webp',
+        small_image_url:
+          'https://cdn.myanimelist.net/images/manga/3/263462t.webp',
+        large_image_url:
+          'https://cdn.myanimelist.net/images/manga/3/263462l.webp',
+      },
+    },
+    approved: true,
+    titles: [
+      {
+        type: 'Default',
+        title: 'Yumemiru Danshi wa Genjitsushugisha',
+      },
+      {
+        type: 'Synonym',
+        title: 'The Dreaming Boy Is a Realist',
+      },
+      {
+        type: 'Japanese',
+        title: '夢見る男子は現実主義者',
+      },
+    ],
+    title: 'Yumemiru Danshi wa Genjitsushugisha',
+    title_english: null,
+    title_japanese: '夢見る男子は現実主義者',
+    title_synonyms: ['The Dreaming Boy Is a Realist'],
+    type: 'Manga',
+    chapters: null,
+    volumes: null,
+    status: 'Publishing',
+    publishing: true,
+    published: {
+      from: '2021-03-26T00:00:00+00:00',
+      to: null,
+      prop: {
+        from: {
+          day: 26,
+          month: 3,
+          year: 2021,
+        },
+        to: {
+          day: null,
+          month: null,
+          year: null,
+        },
+      },
+      string: 'Mar 26, 2021 to ?',
+    },
+    score: 6.54,
+    scored: 6.54,
+    scored_by: 907,
+    rank: 14958,
+    popularity: 4935,
+    members: 3949,
+    favorites: 24,
+    synopsis:
+      'Wataru Sajou, who is deeply in love with his beautiful classmate Aika Natsukawa, approaches her without getting discouraged while having dreams about their mutual love. However, one day, he woke up thinking, "I am not really fit to be together with someone as good as her, huh..." Upon realizing this, Wataru started keeping an appropriate distance from her, much to Aika\'s surprise. "Could it be that he hates me now...?" Did his intentions slip by her because she was getting impatient after arriving at the wrong conclusion?! This is the start of a romcom revolving around two people who just can\'t get their feelings across and both think their love is unrequited! (Source: Hobby Japan, translated)',
+    background: null,
+    authors: [
+      {
+        mal_id: 48951,
+        type: 'people',
+        name: 'Yoshikita, Popuri',
+        url: 'https://myanimelist.net/people/48951/Popuri_Yoshikita',
+      },
+      {
+        mal_id: 54073,
+        type: 'people',
+        name: 'Okemaru',
+        url: 'https://myanimelist.net/people/54073/Okemaru',
+      },
+    ],
+    serializations: [
+      {
+        mal_id: 1537,
+        type: 'manga',
+        name: 'Shounen Ace Plus',
+        url: 'https://myanimelist.net/manga/magazine/1537/Shounen_Ace_Plus',
+      },
+    ],
+    genres: [
+      {
+        mal_id: 4,
+        type: 'manga',
+        name: 'Comedy',
+        url: 'https://myanimelist.net/manga/genre/4/Comedy',
+      },
+      {
+        mal_id: 22,
+        type: 'manga',
+        name: 'Romance',
+        url: 'https://myanimelist.net/manga/genre/22/Romance',
+      },
+    ],
+    explicit_genres: [],
+    themes: [
+      {
+        mal_id: 23,
+        type: 'manga',
+        name: 'School',
+        url: 'https://myanimelist.net/manga/genre/23/School',
+      },
+    ],
+    demographics: [
+      {
+        mal_id: 27,
+        type: 'manga',
+        name: 'Shounen',
+        url: 'https://myanimelist.net/manga/genre/27/Shounen',
+      },
+    ],
+    relations: [
+      {
+        relation: 'Alternative version',
+        entry: [
+          {
+            mal_id: 133724,
+            type: 'manga',
+            name: 'Yumemiru Danshi wa Genjitsushugisha',
+            url: 'https://myanimelist.net/manga/133724/Yumemiru_Danshi_wa_Genjitsushugisha',
+          },
+        ],
+      },
+    ],
+    external: [
+      {
+        name: 'Official Site',
+        url: 'https://comic-walker.com/contents/detail/KDCW_KS13202163010000_68/',
+      },
+      {
+        name: 'Wikipedia',
+        url: 'https://en.wikipedia.org/wiki/Yumemiru_Danshi_wa_Genjitsushugisha',
+      },
+      {
+        name: 'Wikipedia',
+        url: 'https://ja.wikipedia.org/wiki/夢見る男子は現実主義者',
+      },
+    ],
+  },
+};
