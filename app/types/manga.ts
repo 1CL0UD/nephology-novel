2619b6c49c76c803f73d6fef93d78303d37e8a0a
CL_0UD1 <633shalahuddin@gmail.com>
2024-02-04 23:54:48 +0700

    manga data type

---
+++ app/types/manga.ts
@@ -0,0 +1,120 @@
+interface MangaImage {
+  image_url: string;
+  small_image_url: string;
+  large_image_url: string;
+}
+
+interface MangaTitle {
+  type: string;
+  title: string;
+}
+
+interface PublishedDate {
+  from: string;
+  to: string | null;
+  prop: {
+    from: {
+      day: number;
+      month: number;
+      year: number;
+    };
+    to: {
+      day: number | null;
+      month: number | null;
+      year: number | null;
+    };
+  };
+  string: string;
+}
+
+interface Author {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+interface Serialization {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+interface Genre {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+interface Theme {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+interface Demographic {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+interface RelationEntry {
+  mal_id: number;
+  type: string;
+  name: string;
+  url: string;
+}
+
+interface ExternalLink {
+  name: string;
+  url: string;
+}
+
+interface MangaData {
+  data: {
+    mal_id: number;
+    url: string;
+    images: {
+      jpg: MangaImage;
+      webp: MangaImage;
+    };
+    approved: boolean;
+    titles: MangaTitle[];
+    title: string;
+    title_english: string | null;
+    title_japanese: string;
+    title_synonyms: string[];
+    type: string;
+    chapters: number | null;
+    volumes: number | null;
+    status: string;
+    publishing: boolean;
+    published: PublishedDate;
+    score: number;
+    scored: number;
+    scored_by: number;
+    rank: number;
+    popularity: number;
+    members: number;
+    favorites: number;
+    synopsis: string;
+    background: string | null;
+    authors: Author[];
+    serializations: Serialization[];
+    genres: Genre[];
+    explicit_genres: any[]; // Assuming it's an array, you may need to define its type
+    themes: Theme[];
+    demographics: Demographic[];
+    relations: {
+      relation: string;
+      entry: RelationEntry[];
+    }[];
+    external: ExternalLink[];
+  };
+}
+
+export default MangaData;
