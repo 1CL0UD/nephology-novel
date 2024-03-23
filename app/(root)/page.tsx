f88e936599101fffadc12bb69412b59e6fa3fa71
Shalahuddin Abdul Aziz <633shalahuddin@gmail.com>
2024-03-23 17:32:29 +0700

    put latest title

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,6 +10,9 @@ const page = () => {
   return (
     <div className="wrapper">
       <PopularNovelCarousel data={data} />
+      <a href="/latest">
+        <h1 className="text-2xl mb-6">Latest Novels</h1>
+      </a>
       <LatestUpdates data={data} show={8} />
     </div>
   );
